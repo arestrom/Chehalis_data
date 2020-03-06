aadcb8806f37d020021e70dc910a09dd94f70cdc
Are Strom <are.strom@gmail.com>
2020-03-06 08:16:24 -0800

    Corrected gitignore for mobile_import_ui

---
+++ mobile_import/mobile_import_ui.R
@@ -0,0 +1,56 @@
+#=========================================================
+# HTML content definition of header page
+#========================================================
+
+# Define the mobile_import ui
+mobile_import_ui = tags$div(
+  fluidRow(
+    column(
+      width = 6,
+      br(),
+      br(),
+      uiOutput("project_select"),
+      br(),
+      uiOutput("mobile_form_select"),
+      br(),
+      br(),
+      actionButton(inputId = "check_for_new_surveys",
+                   label = "Check for new surveys",
+                   class = "check_for_surveys_buttqn"),
+      actionButton(inputId = "import_mobile",
+                   label = "Sync mobile data to SG local",
+                   class = "import_button"),
+      tippy("<i style='color:#1a5e86;padding-left:8px', class='fas fa-info-circle'></i>",
+            tooltip = glue("You will not be able to sync mobile data to the SG database ",
+                           "until you have checked for new surveys and have verified that ",
+                           "all streams and reaches are present. In other words, the ",
+                           "'Missing streams' and 'Missing reaches' datasets below must ",
+                           "both be empty after clicking on the 'Check for new surveys' ",
+                           "button.")),
+      br(),
+      br(),
+      DT::DTOutput("new_surveys"),
+      br(),
+      br()
+    ),
+    column(
+      width = 6,
+      div(id = "sportswomen_image", img(src = "sportswomen.jpg", width = "80%")),
+      br(),
+      br(),
+      br()
+    )
+  ),
+  fluidRow(
+    column(
+      width = 12,
+      br(),
+      DT::DTOutput("missing_streams"),
+      br(),
+      br(),
+      DT::DTOutput("missing_reaches"),
+      br(),
+      br()
+    )
+  )
+)
