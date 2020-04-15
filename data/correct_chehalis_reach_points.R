2d192c4c563a2c83846229dece99e62cd66036c7
Are Strom <are.strom@gmail.com>
2020-04-15 13:20:28 -0700

    Incremental

--- data/correct_chehalis_reach_points.R
+++ data/correct_chehalis_reach_points.R
@@ -93,13 +93,15 @@ reach_edits = reach_edits %>%
   mutate(lat = as.numeric(st_coordinates(Shape)[,2])) %>%
   st_drop_geometry() %>%
   st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%
-  select(fid, wbid = waterbody_id, wb_name = waterbody_name, wb_display_name = waterbody_display_name,
-         rc_llid = llid, rc_cat_code = cat_code, stid = stream_id, location_id, river_mile, location_name,
+  arrange(waterbody_name, river_mile) %>%
+  mutate(seq_id = as.integer(seq(1, nrow(reach_edits)))) %>%
+  select(seq_id, fid, rc_wbid = waterbody_id, rc_wbname = waterbody_name, rc_wbdisplay_name = waterbody_display_name,
+         rc_llid = llid, rc_cat_code = cat_code, rc_stid = stream_id, location_id, river_mile, location_name,
          location_description, comments) %>%
   st_transform(2927)
 
 #==========================================================================
-# Try to join by nearest_feature
+# Join points to streams by nearest_feature, i.e., stream nearest to point
 #==========================================================================
 
 # Identify the nearest stream to the reach_edit points
@@ -115,18 +117,169 @@ stream = sg_streams %>%
 # Join stream to nearest_stream by nst
 nearest_stream = nearest_stream %>%
   inner_join(stream, by = "nst") %>%
-  select(fid, wbid, waterbody_id, wb_name, waterbody_name, wb_display_name,
+  select(seq_id, fid, rc_wbid, waterbody_id, rc_wbname, waterbody_name, rc_wbdisplay_name,
          waterbody_display_name, rc_llid, llid, rc_cat_code, cat_code,
-         stid, stream_id, gid, location_id, river_mile, location_name,
+         rc_stid, stream_id, gid, location_id, river_mile, location_name,
          location_description, comments, nst)
 
 # Pull out the cases where wbid match and check more carefully
 waterbody_matches = nearest_stream %>%
-  filter(wbid == waterbody_id)
+  filter(rc_wbid == waterbody_id)
 
 # Pull out cases where wbid does not match
 waterbody_no_match = nearest_stream %>%
-  filter(!wbid == waterbody_id)
+  filter(!rc_wbid == waterbody_id) %>%
+  arrange(rc_wbname)
+
+#==========================================================================
+# Inspect cases where waterbody_ids agree. See if any updates needed
+# Result: Looks good...no obvious updates needed
+#==========================================================================
+
+# Check if all llids agree....These are now Ok
+chk_llid = waterbody_matches %>%
+  filter(!rc_llid == llid)
+
+# Pull out data where new points need to be added to location table
+new_points_one_seq_id = waterbody_matches %>%
+  mutate(location_id = trimws(location_id)) %>%
+  filter(is.na(location_id) | location_id == "") %>%
+  pull(seq_id)
+
+# Verify all are unique
+any(duplicated(new_points_one_seq_id))
+
+# Get new locations from reach_edits....will have geometry
+new_points_one = reach_edits %>%
+  filter(seq_id %in% new_points_one_seq_id) %>%
+  select(seq_id, waterbody_id = rc_wbid, waterbody_name = rc_wbname,
+         river_mile_measure = river_mile, location_name, location_description,
+         comments)
+
+# NOTE: Remember to check river_miles for missing values and descriptions
+#       for @ characters....change to At. Do this after combining datasets
+#       one and two
+
+#==========================================================================
+# Inspect cases where waterbody_ids do not agree. Updates wiil be needed.
+#==========================================================================
+
+#========= Step 1, inspect new end_points ==============
+
+# First pull out data where new points need to be added to location table
+new_points_two_seq_id = waterbody_no_match %>%
+  mutate(location_id = trimws(location_id)) %>%
+  filter(is.na(location_id) | location_id == "") %>%
+  pull(seq_id)
+
+# Verify all are unique
+any(duplicated(new_points_two_seq_id))
+
+# Get new locations from reach_edits....will have geometry
+new_points_two = reach_edits %>%
+  filter(seq_id %in% new_points_two_seq_id) %>%
+  select(seq_id, rc_wbid, rc_wbname, rc_llid, river_mile_measure = river_mile,
+         location_name, location_description, comments)
+
+# Get stream identifiers from no-match
+stream_no_match = waterbody_no_match %>%
+  filter(seq_id %in% new_points_two_seq_id) %>%
+  select(seq_id, waterbody_id, waterbody_name, llid)
+
+# Join to new_points_two
+new_points_two = new_points_two %>%
+  inner_join(stream_no_match, by = "seq_id") %>%
+  st_transform(4326) %>%
+  select(seq_id, rc_wbid, waterbody_id, rc_wbname, waterbody_name,
+         rc_llid, llid, river_mile_measure, location_name,
+         location_description, comments) %>%
+  arrange(waterbody_name)
+
+# Manually update comments...will use indicated wbid
+# new_points_two = new_points_two %>%
+  # mutate(use_dat = case_when(                        # No need to use. All are correct on Lea's side
+  #   seq_id %in% c(718, 719, 652, 32, 658, 725, 731
+  #                 722, 713, 715, 260, 724, 729, 438,
+  #                 439, 27, 744, 240) ~ "rc",
+  #   is.na(seq_id) ~ "sg"))
+
+# Comments to Lea
+# 1. Trib 0056, RM 0.7 displays at mouth, 0.0
+#               RM 0.0 dislays off the stream line, may be incorrect GPS coordinates.
+
+# Pull out needed fields from two
+new_points_two = new_points_two %>%
+  st_transform(2927) %>%
+  select(seq_id, waterbody_id = rc_wbid, waterbody_name = rc_wbname,
+         river_mile_measure, location_name, location_description,
+         comments)
+
+# Create location table entries with new end_points
+new_points = rbind(new_points_one, new_points_two)
+
+# Pull out wria table for wria join
+qry = glue("select wr.wria_id, wr.wria_code, wr.geom as geometry ",
+           "from wria_lut as wr ",
+           "where wr.wria_code in ('22', '23')")
+
+# Get values from source
+db_con = pg_con_local(dbname = "spawning_ground")
+wria_st = st_read(db_con, query = qry)
+dbDisconnect(db_con)
+
+# Create new location_id and correct values in rm, desc
+new_points = new_points %>%
+  mutate(location_id = remisc::get_uuid(nrow(new_points))) %>%
+  st_join(wria_st) %>%
+  mutate(location_type_id = "0caa52b7-dfd7-4bf6-bd99-effb17099fd3") %>%               # Reach boundary point
+  mutate(stream_channel_type_id = "540d2361-7598-46b0-88b0-895558760c52") %>%         # Not applicable
+  mutate(location_orientation_type_id = "ffdeeb40-11c8-4268-a2a5-bd73dedd8c25") %>%   # Not applicable
+  mutate(location_code = NA_character_) %>%
+  mutate(waloc_id = NA_integer_) %>%
+  mutate(created_datetime = with_tz(Sys.time(), tzone = "UTC")) %>%
+  mutate(created_by = Sys.getenv("USERNAME")) %>%
+  mutate(modified_datetime = as.POSIXct(NA)) %>%
+  mutate(modified_by = NA_character_) %>%
+  select(seq_id, location_id, waterbody_id, wria_id, location_type_id,
+         stream_channel_type_id, location_orientation_type_id,
+         river_mile_measure, location_code, location_name, location_description,
+         waloc_id, created_datetime, created_by, modified_datetime,
+         modified_by)
+
+# Get the current max_gid from the location_coordinates table
+qry = "select max(gid) from location_coordinates"
+db_con = pg_con_local(dbname = "spawning_ground")
+max_gid = DBI::dbGetQuery(db_con, qry)
+DBI::dbDisconnect(db_con)
+next_gid = max_gid$max + 1
+
+# Pull out data for location_coordinates table
+new_coords = new_points %>%
+  mutate(location_coordinates_id = remisc::get_uuid(nrow(new_points))) %>%
+  mutate(horizontal_accuracy = NA_real_) %>%
+  mutate(comment_text = NA_character_) %>%
+  mutate(gid = seq(next_gid, nrow(new_points) + next_gid - 1)) %>%
+  select(location_coordinates_id, location_id, horizontal_accuracy,
+         comment_text, gid, created_datetime, created_by,
+         modified_datetime, modified_by)
+
+#=========================================================================================
+# Get rid of new points from match and no-match
+# Then check comments to look for any updates needed in location table
+#=========================================================================================
+
+# Pull out the cases where wbid match and check more carefully
+waterbody_matches = waterbody_matches %>%
+  mutate(location_id = trimws(location_id)) %>%
+  filter(!is.na(location_id) & !location_id == "")
+
+# Check on range of tasks needed...manually checked..get rid of "c", and "new".
+unique(waterbody_matches$comments)
+
+# Pull out data where updates are needed
+updates_match = waterbody_matches %>%
+  filter(!is.na(comments)) %>%
+  filter(!comments %in% c("c", "New"))
 
 
 
@@ -144,81 +297,75 @@ waterbody_no_match = nearest_stream %>%
 
 
 
+# Pull out cases where wbid does not match
+waterbody_no_match = waterbody_no_match %>%
+  filter(!is.na(location_id))
 
 
 
 
-# Pull out data with geometry for sg_points
-sg_all = sg_points
-sg_points = sg_points %>%
-  filter(!is.na(gid))
 
-# Check crs
-st_crs(sg_points)$epsg
 
-# Set gid to sequential
-sg_points = sg_points %>%
-  select(waterbody_id, waterbody_name, waterbody_display_name,
-         llid, cat_code, wria_code, stream_id, location_id,
-         river_mile, location_name, location_description,
-         gid, geometry)
 
-# Output as shapefile...this is the only format Arc can open it seems.
-# write_sf(sg_points, dsn = "data/sg_points_2020-04-02.shp", delete_layer = TRUE)
+#=========================================================================================
+# Write all data tables to local spawning_ground
+#=========================================================================================
 
-# Set gid to sequential
-sg_points = sg_points %>%
-  mutate(fid = seq(1, nrow(sg_points))) %>%
-  select(fid, waterbody_id, waterbody_name, waterbody_display_name,
-         llid, cat_code, wria_code, stream_id, location_id,
-         river_mile, location_name, location_description, geometry) %>%
-  st_transform(., 4326)
+#=======================================================
+# Location data...local
+#=======================================================
 
-# Pull out lat lons
-sg_coords = sg_points %>%
-  mutate(longitude = as.numeric(st_coordinates(geometry)[,1])) %>%
-  mutate(latitude = as.numeric(st_coordinates(geometry)[,2])) %>%
-  st_drop_geometry() %>%
-  select(fid, waterbody_id, waterbody_name, waterbody_display_name,
-         llid, cat_code, wria_code, stream_id, location_id,
-         river_mile, location_name, location_description,
-         longitude, latitude)
+# Write locations
+db_con = pg_con_local(dbname = "spawning_ground")
+DBI::dbWriteTable(db_con, "location", location_dt, row.names = FALSE, append = TRUE)
+DBI::dbDisconnect(db_con)
 
-# # Output as a geopackage
-# write_sf(sg_points, "data/sg_points_4326_2020-04-02.gpkg")
+# Write location_coordinates
+db_con = pg_con_local(dbname = "spawning_ground")
+st_write(obj = location_coordinates_dt, dsn = db_con, layer = "location_coordinates_temp")
+DBI::dbDisconnect(db_con)
 
-# Trim to needed columns
-sg_points_no_coords = sg_all %>%
-  st_drop_geometry() %>%
-  filter(is.na(gid)) %>%
-  select(waterbody_id, waterbody_name, waterbody_display_name,
-         llid, cat_code, wria_code, stream_id, location_id,
-         river_mile, location_name, location_description) %>%
-  distinct() %>%
-  arrange(waterbody_name, river_mile)
+# Use select into query to get data into location_coordinates
+qry = glue::glue("INSERT INTO location_coordinates ",
+                 "SELECT CAST(location_coordinates_id AS UUID), CAST(location_id AS UUID), ",
+                 "horizontal_accuracy, comment_text, gid, geometry AS geom, ",
+                 "CAST(created_datetime AS timestamptz), created_by, ",
+                 "CAST(modified_datetime AS timestamptz), modified_by ",
+                 "FROM location_coordinates_temp")
 
-# # Output with styling
-# num_cols = ncol(sg_points_no_coords)
-# current_date = format(Sys.Date())
-# out_name = paste0("data/SGPointsNoCoords_", current_date, ".xlsx")
-# wb <- createWorkbook(out_name)
-# addWorksheet(wb, "SGPointsNoCoords", gridLines = TRUE)
-# writeData(wb, sheet = 1, sg_points_no_coords, rowNames = FALSE)
-# ## create and add a style to the column headers
-# headerStyle <- createStyle(fontSize = 12, fontColour = "#070707", halign = "left",
-#                            fgFill = "#C8C8C8", border="TopBottom", borderColour = "#070707")
-# addStyle(wb, sheet = 1, headerStyle, rows = 1, cols = 1:num_cols, gridExpand = TRUE)
-# saveWorkbook(wb, out_name, overwrite = TRUE)
+# Insert select to spawning_ground
+db_con = pg_con_local(dbname = "spawning_ground")
+DBI::dbExecute(db_con, qry)
+DBI::dbDisconnect(db_con)
 
-# # Output with styling
-# num_cols = ncol(sg_coords)
-# current_date = format(Sys.Date())
-# out_name = paste0("data/SGCoords_", current_date, ".xlsx")
-# wb <- createWorkbook(out_name)
-# addWorksheet(wb, "SGCoords", gridLines = TRUE)
-# writeData(wb, sheet = 1, sg_coords, rowNames = FALSE)
-# ## create and add a style to the column headers
-# headerStyle <- createStyle(fontSize = 12, fontColour = "#070707", halign = "left",
-#                            fgFill = "#C8C8C8", border="TopBottom", borderColour = "#070707")
-# addStyle(wb, sheet = 1, headerStyle, rows = 1, cols = 1:num_cols, gridExpand = TRUE)
-# saveWorkbook(wb, out_name, overwrite = TRUE)
+# Drop temp
+db_con = pg_con_local(dbname = "spawning_ground")
+DBI::dbExecute(db_con, "DROP TABLE location_coordinates_temp")
+DBI::dbDisconnect(db_con)
+
+#============================================================
+# Reset gid_sequence
+
+# Get the current max_gid from the location_coordinates table
+qry = "select max(gid) from location_coordinates"
+db_con = pg_con_local(dbname = "spawning_ground")
+max_gid = DBI::dbGetQuery(db_con, qry)
+DBI::dbDisconnect(db_con)
+
+# Code to reset sequence
+qry = glue("SELECT setval('location_coordinates_gid_seq', {max_gid}, true)")
+db_con = pg_con_local(dbname = "spawning_ground")
+DBI::dbExecute(db_con, qry)
+DBI::dbDisconnect(db_con)
+
+
+
+
+
+
+
+
+
+
+
+
